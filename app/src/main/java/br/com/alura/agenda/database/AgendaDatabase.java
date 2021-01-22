bd9e8c37cb0f62f7194825fea41c2f542bb56bca
pedrindenard <pedrominuzzi21@gmail.com>
2021-01-22 14:03:39 -0300

    Implementando migration

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,9 +1,12 @@
 package br.com.alura.agenda.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import br.com.alura.agenda.model.Aluno;
 
@@ -18,6 +21,18 @@ public abstract class AgendaDatabase extends RoomDatabase {
         return Room
                 .databaseBuilder(context, AgendaDatabase.class, NOME_BANDO_DE_DADOS)
                 .allowMainThreadQueries()
+                .addMigrations(new Migration(1, 2) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                        database.execSQL("ALTER TABLE aluno ADD COLUMN endereço TEXT");
+                    }
+                }, new Migration(2,3) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+
+                    }
+                })
                 .build();
     }
 }

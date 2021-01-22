15d3a764efeb4513f8530c60f9e5c87bd40e4cde
pedrindenard <pedrominuzzi21@gmail.com>
2021-01-22 10:28:05 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -0,0 +1,23 @@
+package br.com.alura.agenda.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import br.com.alura.agenda.model.Aluno;
+
+@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+public abstract class AgendaDatabase extends RoomDatabase {
+
+    private static final String NOME_BANDO_DE_DADOS = "agenda.db";
+
+    public abstract AlunoDAO getRoomAlunoDAO();
+
+    public static AgendaDatabase getInstance(Context context) {
+        return Room
+                .databaseBuilder(context, AgendaDatabase.class, NOME_BANDO_DE_DADOS)
+                .allowMainThreadQueries()
+                .build();
+    }
+}
---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,69 @@
+package br.com.alura.agenda.model;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+
+@Entity
+public class Aluno implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id = 0;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    @Ignore
+    public Aluno(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Aluno() { }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return nome + " - " + telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean temIdValido() {
+        return id > 0;
+    }
+}

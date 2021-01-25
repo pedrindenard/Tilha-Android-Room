47d713bdffe68f58f684026b391023537032dfe7
pedrindenard <pedrominuzzi21@gmail.com>
2021-01-25 10:09:48 -0300

    Aplicando migration das novas identidades

--- app/src/main/java/br/com/alura/agenda/database/AgendaMigrations.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaMigrations.java
@@ -4,6 +4,10 @@ import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.migration.Migration;
 import android.support.annotation.NonNull;
 
+import br.com.alura.agenda.model.TipoTelefone;
+
+import static br.com.alura.agenda.model.TipoTelefone.FIXO;
+
 class AgendaMigrations {
 
     private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
@@ -40,7 +44,38 @@ class AgendaMigrations {
             database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
         }
     };
-    static final Migration[] TODAS_MIGRATIONS = {MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4};
+    private static final Migration MIGRATION_4_5 = new Migration(4, 5) {
+
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_novo` (" +
+                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`nome` TEXT, " +
+                    "`sobrenome` TEXT, " +
+                    "`edereço` TEXT, " +
+                    "`email` TEXT, " +
+                    "`momentoDeCadastro` INTEGER)");
+
+            database.execSQL("INSERT INTO Aluno_novo (id, nome, email, momentoDeCadastro)" +
+                    "SELECT id, nome, email, momentoDeCadastro FROM Aluno");
+
+            database.execSQL("CREATE TABLE IF NOT EXISTS `Telefone` (" +
+                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`numero` TEXT, " +
+                    "`tipo` TEXT, " +
+                    "`alunoId` INTEGER NOT NULL)");
+
+            database.execSQL("INSERT INTO Telefone (numero, alunoId)" +
+                    "SELECT telefoneFixo, id FROM Aluno");
+
+            database.execSQL("UPDATE Telefone SET tipo = ?", new TipoTelefone[] {FIXO});
+
+            database.execSQL("DROP TABLE Aluno");
+
+            database.execSQL("ALTER TABLE Aluno_novo RENAME TO Aluno");
+        }
+    };
+    static final Migration[] TODAS_MIGRATIONS = {MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5};
 }
 
 
